29a966b3bfd1df2909bf6448b4415a082fbee7e4
mhglaptop <mhg@netcompany.com>
2015-04-08 16:01:36 +0200

    Solution

--- src/QueensLogic.java
+++ src/QueensLogic.java
@@ -19,7 +19,7 @@ public class QueensLogic {
     private BDD True;
     private BDD False;
     private BDD bdd;
-    private int N = 8;
+    private int N;
 
 
     public QueensLogic() {
@@ -29,10 +29,90 @@ public class QueensLogic {
     public void initializeGame(int size) {
         this.x = size;
         this.y = size;
+        this.N = size;
         this.board = new int[x][y];
+        System.out.println("This is a " + N + "*" +N+" board");
+
+        //Initialize the factory with a cache
+        this.factory = JFactory.init(2000000, 200000);
+
+        //Create N*N variables.
+        this.factory.setVarNum(this.N*this.N);
+
+        // Create true and false nodes.
+        this.False = this.factory.zero();
+        this.True = this.factory.one();
+
+        //Initalize our bdd to true
+        this.bdd = True;
+
+        for (int i = 0; i< N;i++){
+            for(int j=0; j< N; j++) {
+                boardRules(i,j);
+                System.out.println("Rules added for " +i + ";" +j);
+                System.out.println("Is variable: " + getVariable(i,j));
+            }
+        }
     }
 
-   
+    // Rules taken from http://javabdd.sourceforge.net/xref/NQueens.html
+    // x, y correspond to the specific cell we are looking at.
+    private void boardRules(int x, int y){
+        BDD restFalse = True;
+        BDD cellBDD = False;
+
+        // No queen must be placed in the same row
+        for (int i = 0; i < N; i++){
+            if (i != y) {
+                // All other cells must be false, ie no queen is placed in the cell
+                restFalse = restFalse.and(this.factory.nithVar(getVariable(x,i)));
+            }
+        }
+
+        // No queen must be placed in the same column
+        for (int i = 0; i < N; i++){
+            if (i != x) {
+                // All other cells must be false, ie no queen is placed in the cell
+                restFalse = restFalse.and(this.factory.nithVar(getVariable(i,y)));
+            }
+        }
+
+        int current = getVariable(x,y);
+        // Up left check
+        int uCount = 1;
+        while (uCount <= x) {
+            current-=N+1;
+            if (current >= 0) {
+                restFalse = restFalse.and(this.factory.nithVar(current));
+            }
+            uCount++;
+        }
+
+
+        // Down left check
+        int dCount = 1;
+        current = getVariable(x,y);
+        while (dCount <= x) {
+            current+=N-1;
+            if (current < N*N) {
+                restFalse = restFalse.and(this.factory.nithVar(current));
+            }
+            dCount++;
+        }
+
+        // If a queen is placed in this cell then this most be true
+        cellBDD = cellBDD.or(restFalse);
+        // Otherwise this must be true
+        cellBDD = cellBDD.or(this.factory.nithVar(getVariable(x,y)));
+
+        // This cell must be true for the BBD to be true
+        this.bdd = this.bdd.and(cellBDD);
+    }
+
+    private int getVariable(int x, int y){
+        return N*y+x;
+    }
+
     public int[][] getGameBoard() {
         return board;
     }
@@ -42,11 +122,29 @@ public class QueensLogic {
         if (board[column][row] == -1 || board[column][row] == 1) {
             return true;
         }
-        
+
+        System.out.println("Queen placed in " + column + ":" + row);
+
         board[column][row] = 1;
-        
+
+        // Place the queen in the our BDD
+        this.bdd = this.bdd.restrict(this.factory.ithVar(getVariable(column, row)));
+        System.out.println("Placeing a queen in bdd variable: " + getVariable(column,row));
+
+        System.out.println("Updating rules");
         // put some logic here..
-      
+        // Update board with invalid placements
+        for (int i = 0; i< N;i++){
+            for(int j=0; j< N; j++) {
+                BDD test = this.bdd.restrict(this.factory.ithVar(getVariable(i,j)));
+                if (test.isZero()) {
+                    System.out.println("A queen cannot be placed in field " + i + ":" +j);
+                    System.out.println("Is variable: " + getVariable(i,j));
+                    board[i][j] = -1;
+                }
+            }
+        }
+
         return true;
     }
 }
