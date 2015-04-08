ad1594f47fe850717c74aaa07ab4151236597ffa
mhglaptop <mhg@netcompany.com>
2015-04-08 10:30:54 +0200

    Initial commit

---
+++ src/QueensLogic.java
@@ -0,0 +1,52 @@
+/**
+ * This class implements the logic behind the BDD for the n-queens problem
+ * You should implement all the missing methods
+ * 
+ * @author Stavros Amanatidis
+ *
+ */
+import java.util.*;
+
+import net.sf.javabdd.*;
+
+public class QueensLogic {
+    private int x = 0;
+    private int y = 0;
+    private int[][] board;
+
+
+    private BDDFactory factory;
+    private BDD True;
+    private BDD False;
+    private BDD bdd;
+    private int N = 8;
+
+
+    public QueensLogic() {
+       //constructor
+    }
+
+    public void initializeGame(int size) {
+        this.x = size;
+        this.y = size;
+        this.board = new int[x][y];
+    }
+
+   
+    public int[][] getGameBoard() {
+        return board;
+    }
+
+    public boolean insertQueen(int column, int row) {
+
+        if (board[column][row] == -1 || board[column][row] == 1) {
+            return true;
+        }
+        
+        board[column][row] = 1;
+        
+        // put some logic here..
+      
+        return true;
+    }
+}
---
+++ src/run.ps1
@@ -0,0 +1,2 @@
+javac -cp "javabdd-1.0b2.jar;." *.java
+java -cp "javabdd-1.0b2.jar;." ShowBoard
